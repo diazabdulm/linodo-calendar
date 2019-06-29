990c870d11052fa39f2c3b8edc332377e7188dff
Abdul Diaz <diazabdulm@gmail.com>
2019-06-28 23:02:40 -0400

    reorganized code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import SearchColleges from './components/SearchColleges';
 import NavigationMenu from './components/NavigationMenu';
-import CollegeListItem from './components/CollegeListItem';
+import CollegeList from './components/CollegeList';
 import AddDeadlinesToCalendar from './components/AddDeadlinesToCalendar';
 import './App.css';
 
@@ -10,9 +10,7 @@ function App() {
   return (
     <>
       <NavigationMenu />
-      <Container fluid>
-        <CollegeListItem />
-      </Container>
+      <CollegeList />
       <AddDeadlinesToCalendar />
     </>
   );
